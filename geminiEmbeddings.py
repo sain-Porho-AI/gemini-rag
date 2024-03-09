6de6dd66d7c972a2c435561122742df8d96ae777
MuhammadNizamani <ishaque.niz786@gmail.com>
2024-03-09 18:22:25 +0500

    enh: aaded

---
+++ geminiEmbeddings.py
@@ -0,0 +1,13 @@
+import textwrap
+import numpy as np
+import pandas as pd
+
+import google.generativeai as genai
+import google.ai.generativelanguage as glm
+
+# Used to securely store your API key
+
+from IPython.display import Markdown
+
+from dotenv import load_dotenv
+load_dotenv()
